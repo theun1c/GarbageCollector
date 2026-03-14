b4634d9a6ff4d94f290683836e9601f6d49b0db0
theun1c <belovdima1122@yandex.ru>
2026-03-14 04:05:40 +0300

    chore: add comments

--- app/config.py
+++ app/config.py
@@ -1,8 +1,9 @@
 # хранит токен, путь к базе и прочие настройки
 
-import os
+import os # для чтения из енв системы
 
-BOT_TOKEN = os.getenv('BOT_TOKEN')
+BOT_TOKEN = os.getenv('BOT_TOKEN') # читаем с енв токен
 
+# проверка ошибок
 if not BOT_TOKEN:
     raise RuntimeError('BOT_TOKEN environment variable not set')
--- app/main.py
+++ app/main.py
@@ -1,23 +1,28 @@
 import asyncio # нужен для запуска асинхронной функции main
 
+# Bot - TG API  Dispatcher - маршрутизатор входящих событий
 from aiogram import Bot, Dispatcher
 
+# просто импорты функционала и токена из файлов
 from config import BOT_TOKEN
-from handlers.notes import router
+from handlers.notes import router # импорт роутера с обработчиком команд
 
 # собирает приложение и запускает бота
 
+# главная функция асинхронная
 async def main() -> None:
-    bot = Bot(token=BOT_TOKEN)
+    bot = Bot(token=BOT_TOKEN) # инициализируем бота
     dispatcher = Dispatcher() # создаем диспатчер который принимает события от тг
 
     dispatcher.include_router(router) # подключаем роутер с обработчиками
 
     try:
+        # переключение на поллинг - клиент периодически отправляет запросы
         await bot.delete_webhook(drop_pending_updates=True)
+        # запуск постоянного чтения из тг
         await dispatcher.start_polling(bot)
     finally:
-        await bot.session.close()
+        await bot.session.close()  # гарантия закрытия бота
 
 if __name__ == '__main__': # блок выполняется если файл запущен напрямую
-    asyncio.run(main())
+    asyncio.run(main()) # старт event loop и выполнение main
